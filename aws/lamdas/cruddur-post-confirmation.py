733464acbb984071b2763104995fa5ca5aceca87
Mohammed Adams <madams01@staff.ug.edu.gh>
2023-04-15 19:39:34 +0000

    modify docker compose file

---
+++ aws/lamdas/cruddur-post-confirmation.py
@@ -0,0 +1,43 @@
+import json
+import psycopg2
+import os
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    user_display_name = user["name"]
+    user_email = user["email"]
+    user_handle = user["preferred_username"]
+    user_cognito_id = user["sub"]
+
+    try:
+        conn = psycopg2.connect(os.getenv("CONNECTION_URL"))
+        cur = conn.cursor()
+        sql = f"""
+        "INSERT INTO users (
+            display_name,
+            email, 
+            handle, 
+            cognito_user_id
+            ) 
+        VALUES(
+            {display_name},
+            {email},
+            {handle},
+            {cognito_user_id}
+            )"
+        """
+
+        print(sql)
+        cur.execute(sql)
+        conn.commit()
+        print('Commit Done')
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+    return event
